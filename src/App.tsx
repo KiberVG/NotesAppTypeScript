6dff3d4a791ced82aafae5205ca2e09539234e2a
KiberVG <martinezkimber03@gmail.com>
2024-04-18 17:42:26 -0400

    added more comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Note from './note'
 function App() {
   const [todonotes, setTodonotes] = useState<string []>([])
 
+  // This gets the notes and changes the state
+  // The backend currently returns a just a list of strings
   async function getNotes() {
     const response  = await fetch("http://localhost:8000/notes")
     const data: {notes: string []} = await response.json()
@@ -17,8 +19,11 @@ function App() {
   }
   ,[])
 
+  // This handles when a user decides to add a new note.
   async function handleSubmit(e) {
+    // Preventing page reload
     e.preventDefault()
+    // Getting the user input from the form
     const newNote: string = e.target.name.value
     await fetch("http://localhost:8000/create_note", {
       method: "POST", 
@@ -27,20 +32,22 @@ function App() {
       },
       body: JSON.stringify({'content': newNote}),
     });
+    // Getting all of the notes from the server again after adding new one
     getNotes()
   }
 
   return (
     <>
-    {/* Showing the information only if the user is logged in */}
         <>
           <NavBar />
+
           <div className="notes">
-          {/* Mapping the information from the database to separate notes */}
+            {/* Mapping the information from the database to separate notes */}
             {todonotes.map((element) => (
               <Note content={element} />
             ))}
           </div>
+
           <div>
           <form onSubmit={handleSubmit}>
             <label>Add note:</label>
