9d24908e255ed5f34879dc2ac969474e6bcaaf6b
KiberVG <martinezkimber03@gmail.com>
2024-04-17 23:20:44 -0400

    Added note reading and adding functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,4 @@
-import { useState } from 'react';
-
-// You may have this boolean value after using some authentication
-const loggedIn = true;
-
-// You may have a list of todos from your database after your user logs in.
-const todos = ["Todo 1", "Todo 2", "Todo 3"];
-
-// You may get all of this information in this format from your database
-const todonotes = [
-  { title: "Todo 1", content: "Walk my dog" },
-  { title: "Todo 2", content: "Walk my dog again" },
-  { title: "Todo 3", content: "Remember to walk the dog again" },
-  { title: "Todo 4", content: "Please don't forget to walk the dog" },
-  { title: "Todo 5", content: "Oh no I think I forgot" },
-  { title: "Todo 6", content: "Todo 6 content" },
-  { title: "Todo 7", content: "Todo 7 content" },
-  { title: "Todo 8", content: "Todo 8 content" },
-];
+import { useState, useEffect} from 'react';
 
 // Navbar component
 function NavBar() {
@@ -43,64 +25,70 @@ function NavBar() {
   );
 }
 
-interface Note {
-  title: string,
-  content: string,
-  darkMode: boolean
-}
 
 // Note component, using props to individualize each note.
-function Notes({ title, content, darkMode }: Note) {
+function Note({ content }: {content: string}) {
   const [clicked, setClicked] = useState(false)
+
 
   function handleClick() {
     setClicked(!clicked)
   }
 
   return (
-    <div className={"note " + (darkMode && "dark")} onClick={handleClick}>
-      <h1>{title}</h1>
-      {clicked && <p>{content}</p>}
+    <div className="note" onClick={handleClick}>
+      {!clicked? <h1>Click to show</h1> : <p>{content}</p>}
     </div>
   );
 }
 
 function App() {
+  const [todonotes, setTodonotes] = useState([""])
 
-  const [dark, setDark] = useState(false)
+  async function getNotes() {
+    const response  = await fetch("http://localhost:8000/notes")
+    const data: {notes: string []} = await response.json()
+    setTodonotes(data['notes'])
+  
+  }
+  
+  useEffect( () => {
+    getNotes()
+  }
+  ,[])
 
-  function handleClick() {
-    setDark(!dark)
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const newNote: string = e.target.name.value
+    await fetch("http://localhost:8000/create_note", {
+      method: "POST", 
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({'content': newNote}),
+    });
+    getNotes()
   }
 
   return (
     <>
     {/* Showing the information only if the user is logged in */}
-      {loggedIn && (
         <>
           <NavBar />
           <div className="notes">
           {/* Mapping the information from the database to separate notes */}
             {todonotes.map((element) => (
-              <Notes title={element.title} darkMode={dark} content={element.content} />
+              <Note content={element} />
             ))}
           </div>
           <div>
-            <h1>Todo List</h1>
-            {/* Mapping todos to their own <li> tag. */}
-            <ul>
-              {todos.map((element) => (
-                <li>{element}</li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <button onClick={handleClick}>
-              Dark Mode
-            </button>
+          <form onSubmit={handleSubmit}>
+            <label>Add note:</label>
+            <input type="text" id="name" name="name"/>
+            <button type="submit">Submit</button>
+          </form>
           </div>
         </>
-      )}
     </>
   );
 }
